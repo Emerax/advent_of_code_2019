cf82f8056c52ee6ac1f0e5ba416ab8fb328f10c1
Erik <e.naaru@hotmail.com>
2019-12-02 15:25:23 +0100

    Finish day2

---
+++ day2/day2/IntCodeParser.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+public class IntCodeParser {
+    private readonly List<int> program = new List<int>();
+
+    private void Add(int term1Pos, int term2Pos, int sumPos) {
+        int term1 = program[term1Pos];
+        int term2 = program[term2Pos];
+
+        program[sumPos] = term1 + term2;
+    }
+
+    private void Multiply(int factor1Pos, int factor2Pos, int productPos) {
+        int factor1 = program[factor1Pos];
+        int factor2 = program[factor2Pos];
+
+        program[productPos] = factor1 * factor2;
+    }
+
+    public void LoadProgram(string filePath) {
+        program.Clear();
+        string programString = File.ReadAllText(filePath);
+        foreach (string valueString in programString.Split(",")) {
+            program.Add(int.Parse(valueString));
+        }
+    }
+
+    public void LoadProgram(int[] array) {
+        program.Clear();
+        foreach (int value in array) {
+            program.Add(value);
+        }
+    }
+
+    public void SetNounAndVerb(int noun, int verb) {
+        program[1] = noun;
+        program[2] = verb;
+    }
+
+    public void PrintProgram() {
+        StringBuilder sb = new StringBuilder();
+        foreach(int value in program) {
+            sb.Append(value).Append(",");
+        }
+        Console.WriteLine($"Program: {sb.ToString()}");
+    }
+
+    public int GetOutput(int address) {
+        return program[address];
+    }
+
+    public void ParseProgram() {
+        int programCounter = 0;
+        int opCode = program[programCounter];
+
+        while(opCode != 99) {
+            switch (opCode) {
+                case 1:
+                    Add(program[++programCounter], program[++programCounter], program[++programCounter]);
+                    break;
+                case 2:
+                    Multiply(program[++programCounter], program[++programCounter], program[++programCounter]);
+                    break;
+                default:
+                    return;
+            }
+            opCode = program[++programCounter];
+        }
+    }
+}
--- day2/day2/Program.cs
+++ day2/day2/Program.cs
@@ -2,8 +2,36 @@
 
 namespace day2 {
     class Program {
+        private static readonly string filePath = "C:\\Users\\enaar\\advent_of_code_2019\\day2\\program.txt";
+        private static readonly int soughtResult = 19690720;
+
         static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+            IntCodeParser parser = new IntCodeParser();
+
+            //Part1(parser);
+            Part2(parser);
+        }
+
+        static void Part2(IntCodeParser parser) {
+            for(int noun = 0; noun <= 99; ++noun) {
+                for(int verb = 0; verb <= 99; ++verb) {
+                    parser.LoadProgram(filePath);
+                    parser.SetNounAndVerb(noun, verb);
+                    parser.ParseProgram();
+                    Console.WriteLine($"Noun: {noun}, Verb: {verb}, Output: {parser.GetOutput(0)}");
+                    if(parser.GetOutput(0) == soughtResult) {
+                        Console.WriteLine($"Found {soughtResult}! Noun: {noun}, verb: {verb}. Answer is: {100 * noun + verb}");
+                        return;
+                    }
+                }
+            }
+        }
+
+        static void Part1(IntCodeParser parser) {
+            parser.LoadProgram(filePath);
+            parser.SetNounAndVerb(12, 2);
+            parser.ParseProgram();
+            parser.PrintProgram();
         }
     }
 }
